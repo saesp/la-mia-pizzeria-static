a8f1ba34d1d94f270cd21788d2a2acd03ab4780e
Salvatore Esposito <esposito.00@outlook.it>
2023-04-29 16:41:10 +0200

    added pizza list + changed style with bootstrap

--- la-mia-pizzeria-static/Controllers/HomeController.cs
+++ la-mia-pizzeria-static/Controllers/HomeController.cs
@@ -2,18 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-//Dobbiamo realizzare un’applicazione web che ci aiuti a gestire la nostra pizzeria.
-//Abbiamo bisogno di creare la nostra prima pagina (index) dove mostriamo tutte le pizze che prepariamo.
-//Una pizza avrà le seguenti informazioni :
-//-un nome
-//- una descrizione
-//- una foto
-//- un prezzo
-//Modifichiamo quindi la view Index.cshtml generata in automatico da .Net Core scrivendo l’html che serve a noi per mostrare l’elenco delle pizze (possiamo creare una tabella con bootstrap o una qualche grafica a nostro piacimento che mostri l’elenco delle pizze...proviamo un po’ di creatività se vogliamo!)
-//Piccolo suggerimento : ricordatevi di inserire il seguente codice all’inizio della vostra view
-//@ {
-//      Layout = null;
-//}
 
 namespace la_mia_pizzeria_static.Controllers
 {
